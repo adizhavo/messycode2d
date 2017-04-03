f96595fe148e51082aa7ed6248570d03d78a9b0f
Adi Zhavo <adi@gram.gs>
2017-04-03 19:14:55 +0100

    Add scene class

---
+++ MessyCode2D/src/scene/scene.cpp
@@ -0,0 +1,18 @@
+#include "scene.hpp"
+
+namespace MessyCode2D_Engine {
+    void Scene::Build()
+    {
+    
+    }
+    
+    void Scene::Boot()
+    {
+    
+    }
+    
+    void Scene::Feed()
+    {
+    
+    }
+}
---
+++ MessyCode2D/src/scene/scene.hpp
@@ -0,0 +1,13 @@
+#ifndef scene_hpp
+#define scene_hpp
+
+namespace MessyCode2D_Engine {
+    class Scene
+    {
+    public:
+        void Build();
+        void Boot();
+        void Feed();
+    };
+}
+#endif
