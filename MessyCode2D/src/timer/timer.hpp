f9f4bf3fdf28de486b3544aa6b0503845a375d5f
Adi Zhavo <adi@gram.gs>
2017-04-03 20:04:24 +0100

    Add timer

---
+++ MessyCode2D/src/timer/timer.hpp
@@ -0,0 +1,20 @@
+#ifndef timer_hpp
+#define timer_hpp
+
+class Timer {
+public:
+    Timer();
+    void           start();
+    void           stop();
+    void           reset();
+    bool           isRunning();
+    unsigned long  getTime();
+    bool           isOver(unsigned long seconds);
+private:
+    bool           resetted;
+    bool           running;
+    unsigned long  beg;
+    unsigned long  end;
+};
+
+#endif
