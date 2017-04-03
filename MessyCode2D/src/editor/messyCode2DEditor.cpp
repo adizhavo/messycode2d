9d408d8c56076716c89ec17e4f53deaf67dc8b35
Adi Zhavo <adi@gram.gs>
2017-04-03 20:08:12 +0100

    Add logs in the editor class

--- MessyCode2D/src/editor/messyCode2DEditor.cpp
+++ MessyCode2D/src/editor/messyCode2DEditor.cpp
@@ -1,8 +1,9 @@
 #include "messyCode2DEditor.hpp"
+#include <iostream>
 
 namespace MessyCode2D_Engine {
     void MessyCode2DEditor::Feed()
     {
-    
+        std::cout << "Editor feeded\n";
     }
 }
