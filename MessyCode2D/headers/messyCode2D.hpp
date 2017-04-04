cf47783ea629c660258e0eb6c7783b3d35584f3f
Adi Zhavo <adi@gram.gs>
2017-04-04 17:03:02 +0100

    Add config file for the engine

--- MessyCode2D/headers/messyCode2D.hpp
+++ MessyCode2D/headers/messyCode2D.hpp
@@ -1,23 +1,18 @@
 #ifndef messyCode2D_hpp
 #define messyCode2D_hpp
 
-#include "scene.hpp"
 #include "messyCode2DEditor.hpp"
+#include "messyCode2DConfig.hpp"
+#include "scene.hpp"
 #include "timer.hpp"
 
 namespace MessyCode2D_Engine {
-    class MessyCodeConfig
-    {
-    public:
-        float framePerSec = 30;
-    };
-    
     class MessyCode2D
     {
     private:
         MessyCodeConfig* config;
-        Scene* scene;
         MessyCode2DEditor* editor;
+        Scene* scene;
         Timer* timer;
         
         bool ShoulFeed();
---
+++ MessyCode2D/headers/messyCode2DConfig.hpp
@@ -0,0 +1,12 @@
+#ifndef messyCode2DConfig_hpp
+#define messyCode2DConfig_hpp
+
+namespace MessyCode2D_Engine {
+    class MessyCodeConfig
+    {
+    public:
+        float framePerSec = 30;
+    };
+}
+
+#endif
