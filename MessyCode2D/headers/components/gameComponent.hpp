6f12f56b014da859349b68226644641e3be50297
Adi Zhavo <adi@gram.gs>
2017-04-05 12:42:08 +0100

    Add isEnabled flag in the component

--- MessyCode2D/headers/components/gameComponent.hpp
+++ MessyCode2D/headers/components/gameComponent.hpp
@@ -6,10 +6,11 @@
 namespace MessyCode2D_Engine {
     CREATE_COMPONENT(GameComponent)
 public:
+    bool isEnabled = true;
     bool hasStarted = false;
     
     virtual void Start() = 0;
-    virtual void Update(float elapseTime) = 0;
+    virtual void Update(float deltaTime) = 0;
     virtual void Destroy() = 0;
     
     void Cleanup()
