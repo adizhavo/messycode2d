af7285a371021caaed00885cd16594dff20b191d
Adi Zhavo <adi@gram.gs>
2017-04-16 01:03:28 +0100

    Made the engine configuration class a module

--- headers/messyCode2DConfig.hpp
+++ headers/messyCode2DConfig.hpp
@@ -1,11 +1,22 @@
 #ifndef messyCode2DConfig_hpp
 #define messyCode2DConfig_hpp
 
+#include "messyModule.hpp"
+
 namespace MessyCode2D_Engine {
-    class MessyCodeConfig
+    class MessyCodeConfig : public MessyModule
     {
     public:
-        float framePerSec = 30;
+        float framePerSec;
+
+        void Boot()
+        {
+            framePerSec = 30;
+        }
+
+        void Start() { }
+        void Update(float deltaTime) { }
+        ~MessyCodeConfig() { }
     };
 }
 
