a1e3c9eb6421ce0f5093bcf3556628697c38a447
Adi Zhavo <adi@gram.gs>
2017-05-01 17:36:04 +0100

    Rename editor inspector source file

---
+++ MessyCode2D/src/transform.cpp
@@ -0,0 +1,74 @@
+#include "transform.hpp"
+#include "messyCode2D.hpp"
+#include "hierarchy.hpp"
+#include "entity.hpp"
+
+namespace MessyCode2D_Engine {
+    Transform::Transform()
+    {
+        this->xPos = 0;
+        this->yPos = 0;
+        this->xAngle = 0;
+        this->yAngle = 0;
+        this->xScale = 0;
+        this->yScale = 0;
+        this->parent = NULL;
+    }
+
+    Transform::~Transform()
+    {
+        if (this->parent != NULL)
+        {
+            this->parent->RemoveChild(this);
+            this->parent = 0;
+        }
+
+        for (Transform* tr : this->childs)
+        {
+            delete tr;
+        }
+
+        this->childs.clear();
+        this->entity->RemoveComponent(unique_id());
+
+        MessyCode2D::GetModule<Hierarchy>()->Refresh();
+    }
+
+    void Transform::Start() { }
+
+    void Transform::Update(float deltaTime) { }
+    
+    void Transform::SetParent(Transform* parent)
+    {
+        if (this->parent != NULL)
+            this->parent->RemoveChild(this);
+        
+        this->parent = parent;
+        
+        if (this->parent != NULL)
+            this->parent->AddChild(this);
+
+        MessyCode2D::GetModule<Hierarchy>()->Refresh();
+    }
+    
+    Transform* Transform::GetParent()
+    {
+        return this->parent;
+    }
+    
+    void Transform::AddChild(Transform* child)
+    {
+        this->childs.push_back(child);
+    }
+    
+    void Transform::RemoveChild(Transform* child)
+    {
+        this->childs.erase(std::remove(this->childs.begin(), this->childs.end(), child), this->childs.end());
+        child->SetParent(NULL);
+    }
+    
+    std::vector<Transform*> Transform::GetChildren()
+    {
+        return this->childs;
+    }
+}
