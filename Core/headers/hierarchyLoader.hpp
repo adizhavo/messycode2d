9d9451a087b8ac4640435c4e1bb2cff2fa066ad4
Adi Zhavo <adi@gram.gs>
2017-05-27 20:52:30 +0100

    Fix hierarchy loader to correctly load data

--- Core/headers/hierarchyLoader.hpp
+++ Core/headers/hierarchyLoader.hpp
@@ -1,39 +1,26 @@
 #ifndef HIERARCHYDATALOADER_H
 #define HIERARCHYDATALOADER_H
 
-#include "messyModule.hpp"
-#include <fstream>
 #include <vector>
-
 #include <string>
-#include "messySerializer.hpp"
 
 namespace MessyCode2D_Engine {
-
     // loads and builds the hierarchy
+
+    class HierarchyLoader
+    {
+    public:
+        void LoadHierarchy();
+    };
 
     class EntityData
     {
-        // data to recreate an entity
-
+    // data to recreate an entity
     public:
         int id;
         int parentId;
         std::string name;
         std::vector<std::string> componentsId;
-    };
-
-    class HierarchyLoader : public MessyModule
-    {
-    public:
-        void Boot();
-        void Start();
-        void Update(float elapseTime);
-        void LoadHierarchy();
-        ~HierarchyLoader();
-
-    private:
-        std::ifstream* reader;
     };
 }
 
--- Core/src/hierarchyLoader.cpp
+++ Core/src/hierarchyLoader.cpp
@@ -2,58 +2,38 @@
 #include "messyCode2d.hpp"
 #include "messyConfig.hpp"
 #include <json.hpp>
-#include <cstddef>
-
-#include <QDebug>
+#include <fstream>
 #include <QString>
-#include <iostream>
+#include <QDebug>
 
 namespace MessyCode2D_Engine {
     using namespace std;
     using namespace nlohmann;
-
-    HierarchyLoader::~HierarchyLoader()
-    {
-        delete reader;
-    }
-
-    void HierarchyLoader::Boot()
-    {
-        reader = new ifstream();
-    }
-
-    void HierarchyLoader::Start() { }
-
-    void HierarchyLoader::Update(float elapseTime) { }
 
     void HierarchyLoader::LoadHierarchy()
     {
         MessyCodeConfig* config = MessyCode2D::GetModule<MessyCodeConfig>();
         if (config != NULL)
         {
-            reader->open(config->hierarchyFilePath);
-            std::stringstream buffer;
-            buffer << reader->rdbuf();
+            ifstream reader(config->hierarchyFilePath);
+            if (reader.good())
+            {
+                qDebug() << "[HierarchyLoader] Loaded hierarchy data file";
+                json h_data;
+                reader >> h_data;
+                json e_data = h_data["entities"];
+
+                // Load entity data
+                for (auto& entity : e_data) {
+                    EntityData e;
+                    e.id = entity.at("id").get<int>();
+                    e.parentId = entity.at("parentId").get<int>();
+                    e.name = entity.at("name").get<std::string>();
+                    e.componentsId = entity.at("componentsId").get<vector<string>>();
+                }
+            }
+            else
+            qCritical() << "[HierarchyLoader] Could not load hierarchy, specified file is missing " << QString::fromStdString(config->hierarchyFilePath) ;
         }
-
-        std::cout << "some guys";
-
-        // this is for testing
-//        json h_json = "{ \"entities\" : [ { \"id\" : 1, \"parentId\" : -1, \"name\" : \"test\", \"componentsId\" : [ \"TRANSFORM\" ] } ] }"_json;
-
-//        json e_data = h_json["entities"];
-        // log e_data.size()
-
-//        std::cout << e_data.size();
-
-//        for (auto& entity : e_data) {
-//            EntityData e;
-//            e.id = entity.at("id").get<int>();
-//            e.parentId = entity.at("parentId").get<int>();
-//            e.name = entity.at("name").get<std::string>();
-//            e.componentsId = entity.at("componentsId").get<vector<string>>();
-
-
-//        }
     }
 }
