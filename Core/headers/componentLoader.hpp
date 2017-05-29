3fb8cdfd7f5c6ae1d5874732416298826818dd71
Adi Zhavo <adi@gram.gs>
2017-05-29 20:31:28 +0100

    Component loader return the component if for the scene json

--- Core/headers/componentLoader.hpp
+++ Core/headers/componentLoader.hpp
@@ -13,6 +13,9 @@ namespace MessyCode2D_Engine {
     {
     public:
         Component* GetComponent(const std::string componentId);
+        Component* GetEngineComponent(const std::string componentId);
+        // Insert the COMP_Id from the ECS library
+        std::string GetJsonComponentId(long compId);
     };
 }
 
